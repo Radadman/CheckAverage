a333b79e8c38f00fc48f7ab5f3e462993e94a7c2
Radadman <abdpilot@gmail.com>
2017-04-21 13:33:35 -0500

    Update CountChecks.sql

--- CountChecks.sql
+++ CountChecks.sql
@@ -1,3 +1,7 @@
-SELECT count(distinct concat(LocationID, convert(int,DOB,0)),CheckNumber)
+
+--Get Count of Checks that Have no catering items
+SELECT count(distinct concat(LocationID, convert(int,DOB,0),CheckNumber)
 FROM ItemDetail
 WHERE DOB between 4/3/2017 and 4/9/2017
+
+--Get Total Net Sales of all checks that do not include catering
