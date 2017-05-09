76ffc2e8a521486c5f532281914b580b76c05338
Radadman <abdpilot@gmail.com>
2017-05-09 17:23:03 -0500

    Update Checkswithitems.sql

--- Checkswithitems.sql
+++ Checkswithitems.sql
@@ -1,6 +1,6 @@
 --Select only checks that have sandwich and drink
 Select distinct concat(exp.LocationID, convert(int,exp.DOB,0), exp.CheckNumber) AS CheckID, SUM(exp.GrossPrice) AS GrossPrice, SUM(exp.NetPrice) AS NetPrice
-    From (Select id.* From ItemGroupMember igm INNER JOIN ItemDetail id ON igm.ItemID = id.ItemID WHERE id.DOB BETWEEN @StartDate AND @EndDate AND id.LocationID = 22) exp
+    From (Select id.* From ItemDetail id WHERE id.DOB BETWEEN @StartDate AND @EndDate AND id.LocationID = 22) exp
     Where
     exists (Select e1.ItemGroupID From (Select id.*, igm.ItemGroupID From ItemGroupMember igm INNER JOIN ItemDetail id ON igm.ItemID = id.ItemID WHERE id.DOB BETWEEN @StartDate AND @EndDate AND id.LocationID = 22 AND igm.ItemGroupID in (281)) e1 Where concat(e1.LocationID, convert(int,e1.DOB,0), e1.CheckNumber) = concat(exp.LocationID, convert(int,exp.DOB,0), exp.CheckNumber))
     and exists (Select e1.ItemGroupID From (Select id.*, igm.ItemGroupID From ItemGroupMember igm INNER JOIN ItemDetail id ON igm.ItemID = id.ItemID WHERE id.DOB BETWEEN @StartDate AND @EndDate AND id.LocationID = 22 and igm.ItemGroupID in (274)) e1 Where concat(e1.LocationID, convert(int,e1.DOB,0), e1.CheckNumber) = concat(exp.LocationID, convert(int,exp.DOB,0), exp.CheckNumber))
@@ -22,3 +22,11 @@ WHERE exists (Select e1.LocationID, e1.DOB, e1.CheckNumber FROM (Select id.* FRO
 and id.DOB BETWEEN @StartDate and @EndDate and LocationID = 22) e1 WHERE concat(e1.LocationID, convert(int,e1.DOB,0), e1.CheckNumber) = concat(ch.LocationID, convert(int,ch.DOB,0), ch.CheckNumber))
 GROUP BY ch.LocationID, ch.DOB, ch.CheckNumber
 ORDER BY CheckID
+
+--Select only checks that have sandwich (no catering)
+SELECT distinct concat(exp.LocationID, convert(int,exp.DOB,0), exp.CheckNumber) AS CheckID, SUM(exp.GrossPrice) AS GrossPrice, SUM(exp.NetPrice) AS NetPrice
+FROM (Select id.* FROM ItemDetail id INNER JOIN ItemGroupMember igm ON id.ItemID = igm.ItemID Where DOB Between @StartDate and @EndDate and igm.ItemGroupID = 281) exp
+WHERE 
+DOB BETWEEN @StartDate and @EndDate
+AND GrossPrice > 4.5
+GROUP BY exp.LocationID, exp.DOB, exp.CheckNumber
