2b7ad12d79205148bf6745d01adcdd5c7049dbe5
Radadman <abdpilot@gmail.com>
2017-06-13 17:32:52 -0500

    Create Checkswithitems.sql

--- Checkswithitems.sql
+++ Checkswithitems.sql
@@ -76,7 +76,7 @@ AND exp.LocationGroupID = 1782
 GROUP BY exp.LocationID, exp.DOB
 
 -- Count Items where check has a combo
-Select count(exp.Quantity)
+Select sum(exp.Quantity)
 From (Select id.*, igm.ItemGroupID From ItemDetail id INNER JOIN ItemGroupMember igm ON id.ItemID = igm.ItemID WHERE id.DOB BETWEEN '@StartDate' AND '@EndDate') exp
     Where
     exists (Select e1.ItemGroupID From (Select id.*, igm.ItemGroupID From ItemGroupMember igm INNER JOIN ItemDetail id ON igm.ItemID = id.ItemID WHERE id.DOB BETWEEN '@StartDate' AND '@EndDate' AND igm.ItemGroupID in (302) AND id.GrossPrice >1) e1 Where concat(e1.LocationID, convert(int,e1.DOB,0), e1.CheckNumber) = concat(exp.LocationID, convert(int,exp.DOB,0), exp.CheckNumber))
