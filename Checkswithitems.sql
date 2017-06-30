be226d8a05bd41b8fbdafb709a9e8f9d4fd05a5b
Radadman <abdpilot@gmail.com>
2017-06-30 12:07:16 -0500

    Update Checkswithitems.sql

--- Checkswithitems.sql
+++ Checkswithitems.sql
@@ -105,3 +105,35 @@ INNER JOIN SaleDepartment sd ON it.SaleDepartmentID = sd.SaleDepartmentID and sd
 WHERE DOB BETWEEN '@StartDate' and '@EndDate' and id.GrossPrice > 1) cat WHERE concat(cat.LocationID,convert(int,cat.DOB,0), cat.CheckNumber) = concat(exp.LocationID, convert(int,exp.DOB,0), exp.CheckNumber))
 AND exp.LocationGroupID = 1
 GROUP BY exp.LocationID, exp.LocationName, exp.DOB
+
+-- Bread Use
+SELECT id.LocationID, ig.ItemGroupName, min(convert(varchar(10),id.DOB,110)) as StartDate, max(convert(varchar(10),id.DOB,110)) as EndDate,SUM(id.Quantity) as Sandwiches, 
+CASE When igm.ItemGroupID = 127 Then Round(((SUM(id.Quantity) * 7)/21),2) 
+When igm.ItemGroupID = 128 Then Round(((Sum(id.Quantity)*10.5)/21),2)
+When igm.ItemGroupID = 129 Then Round(((Sum(id.Quantity)*14)/21),2)
+When igm.ItemGroupID = 261 Then Round(((Sum(id.Quantity)*3.5)/21),2)
+When igm.ItemGroupID = 314 Then Round(((Sum(id.Quantity)*5)/21),2)
+When igm.ItemGroupID = 130 Then Round(((Sum(id.Quantity)*10*7)/21),2)
+When igm.ItemGroupID = 282 Then Round(((Sum(id.Quantity)*15*7)/21),2)
+When igm.ItemGroupID = 131 Then Round(((Sum(id.Quantity)*6*7)/21),2)
+Else Round(((Sum(id.Quantity)*23)/21),2)
+END as Loaves,
+CASE When igm.ItemGroupID = 127 Then Round((((SUM(id.Quantity) * 7)/21)/24),2) 
+When igm.ItemGroupID = 128 Then Round((((Sum(id.Quantity)*10.5)/21)/24),2)
+When igm.ItemGroupID = 129 Then Round((((Sum(id.Quantity)*14)/21)/24),2)
+When igm.ItemGroupID = 261 Then Round((((Sum(id.Quantity)*3.5)/21)/24),2)
+When igm.ItemGroupID = 314 Then Round((((Sum(id.Quantity)*5)/21)/24),2)
+When igm.ItemGroupID = 130 Then Round((((Sum(id.Quantity)*10*7)/21)/24),2)
+When igm.ItemGroupID = 282 Then Round((((Sum(id.Quantity)*15*7)/21)/24),2)
+When igm.ItemGroupID = 131 Then Round((((Sum(id.Quantity)*6*7)/21)/24),2)
+Else Round((((Sum(id.Quantity)*23)/21)/24),2)
+END as Cases
+FROM ItemDayTotal id
+INNER JOIN ItemGroupMember igm ON id.ItemID = igm.ItemID
+INNER JOIN ItemGroup ig ON igm.ItemGroupID = ig.ItemGroupID
+INNER JOIN LocationGroupMember lgm ON id.LocationID = lgm.LocationID 
+WHERE id.DOB Between '@StartDate' and '@EndDate'
+AND igm.ItemGroupID IN (127, 128, 129, 261, 314, 130, 282, 131, 246)
+AND lgm.LocationGroupID = '@LocationGroupID'
+GROUP BY id.LocationID, ig.ItemGroupName, igm.ItemGroupID
+
